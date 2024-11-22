c92905c9b30a6761384f65ed1e083e2df2510d6a
nemro <mnabeel.bese22seecs.seecs.edu.pk>
2024-11-23 01:18:40 +0500

    sql

--- database/setup.sql
+++ database/setup.sql
@@ -22,3 +22,25 @@ CREATE TABLE attendance (
     session_id INT REFERENCES sessions(id),
     status BOOLEAN NOT NULL
 );
+CREATE TABLE IF NOT EXISTS "attendance" (
+  "classid" INT NOT NULL,
+  "studentid" INT NOT NULL,
+  "isPresent" BOOLEAN NOT NULL,
+  "comments" VARCHAR(200) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS "class" (
+  "id" SERIAL PRIMARY KEY,
+  "teacherid" INT NOT NULL,
+  "starttime" TIME NOT NULL,
+  "endtime" TIME NOT NULL,
+  "credit_hours" INT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS "user" (
+  "id" SERIAL PRIMARY KEY,
+  "fullname" VARCHAR(200) NOT NULL,
+  "email" VARCHAR(200) NOT NULL,
+  "class" VARCHAR(10) NOT NULL,
+  "role" VARCHAR(50) NOT NULL
+);
